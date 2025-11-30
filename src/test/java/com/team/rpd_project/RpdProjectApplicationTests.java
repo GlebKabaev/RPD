cf01ddc8403952b5d87f1dffaf50275e292193c1
Your Name <147653545+Vishnyakov06@users.noreply.github.com>
2025-11-30 14:48:34 +0300

    создание подгрузки файла

---
+++ src/test/java/com/team/rpd_project/RpdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.rpd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
